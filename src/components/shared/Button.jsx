fe7804ada30fc0e775fe276c17282c189901c7e8
mosh-d <shopsy8@gmail.com>
2025-07-29 18:06:49 +0100

    Fixing routing issue on github pages

--- src/components/shared/Button.jsx
+++ src/components/shared/Button.jsx
@@ -3,9 +3,9 @@ import styled from "styled-components";
 const StyledButton = styled.button`
   box-sizing: border-box;
   width: fit-content;
-  
+
   background-color: ${({ $type }) =>
-      $type === "ghost"
+    $type === "ghost"
       ? "none"
       : $type === "accent-ghost"
       ? "none"
@@ -24,83 +24,82 @@ const StyledButton = styled.button`
       : "none"};
 
   & p {
-      color: ${({ $type }) =>
+    color: ${({ $type }) =>
       $type === "accent-ghost"
-      ? "var(--cordis-accent)"
-      : $type === "underlined"
-      ? "var(--cordis-black)"
-      : $type === "white-ghost"
-      ? "var(--cordis-white)"
-      : "var(--cordis-black)"};
+        ? "var(--cordis-accent)"
+        : $type === "underlined"
+        ? "var(--cordis-black)"
+        : $type === "white-ghost"
+        ? "var(--cordis-white)"
+        : "var(--cordis-black)"};
   }
 
   padding: 1rem 2rem;
   outline: ${({ $type }) =>
-    $type === "accent-ghost" 
-    ? "2px solid var(--cordis-accent)"
-    : $type === "ghost"
-    ? "1px solid var(--cordis-black)"
-    : $type === "white-ghost" 
-    ? "2px solid var(--cordis-white)"
-    : $type === "emphasis-ghost"
-    ? "1px solid var(--cordis-black)"
-    : "none"};
+    $type === "accent-ghost"
+      ? "2px solid var(--cordis-accent)"
+      : $type === "ghost"
+      ? "1px solid var(--cordis-black)"
+      : $type === "white-ghost"
+      ? "2px solid var(--cordis-white)"
+      : $type === "emphasis-ghost"
+      ? "1px solid var(--cordis-black)"
+      : "none"};
   border-bottom: ${({ $type }) =>
-    $type === "underlined"
-    ? "2px solid var(--cordis-emphasis)"
-    : "none"};
+    $type === "underlined" ? "2px solid var(--cordis-emphasis)" : "none"};
 
   &:hover {
-    transition: scale 0.2s ease-in-out, background-color 0.3s ease-in-out, color 0.1s ease-in-out, outline 0.3s ease-in-out;
+    transition: scale 0.2s ease-in-out, background-color 0.3s ease-in-out,
+      color 0.1s ease-in-out, outline 0.3s ease-in-out;
 
     background-color: ${({ $type }) =>
       $type === "accent-ghost"
-      ? "var(--cordis-accent)"
-      : $type === "emphasis-ghost"
-      ? "var(--cordis-emphasis)"
-      : $type === "ghost"
-      ? "var(--cordis-black)"
-      : $type === "white-ghost"
-      ? "var(--cordis-white)"
-      : $type === "underlined"
-      ? "var(--cordis-emphasis)"
-      : $type === "emphasis"
-      ? "rgba(255, 255, 255, 0)"
-      : "rgba(255, 255, 255, 0)"};
+        ? "var(--cordis-accent)"
+        : $type === "emphasis-ghost"
+        ? "var(--cordis-emphasis)"
+        : $type === "ghost"
+        ? "var(--cordis-black)"
+        : $type === "white-ghost"
+        ? "var(--cordis-white)"
+        : $type === "underlined"
+        ? "var(--cordis-emphasis)"
+        : $type === "emphasis"
+        ? "rgba(255, 255, 255, 0)"
+        : "rgba(255, 255, 255, 0)"};
 
     & p {
       color: ${({ $type }) =>
         $type === "accent-ghost"
-        ? "var(--cordis-black)"
-        : $type === "ghost"
-        ? "var(--cordis-white)"
-        : $type === "underlined"
-        ? "var(--cordis-black)"
-        : $type === "emphasis"
-        ? "var(--cordis-emphasis)"
-        : $type === "emphasis2"
-        ? "var(--cordis-black)"
-        : $type === "accent"
-        ? "var(--cordis-accent)"
-        : $type === "white"
-        ? "var(--cordis-white)"
-        : "var(--cordis-black)"};
+          ? "var(--cordis-black)"
+          : $type === "ghost"
+          ? "var(--cordis-white)"
+          : $type === "underlined"
+          ? "var(--cordis-black)"
+          : $type === "emphasis"
+          ? "var(--cordis-emphasis)"
+          : $type === "emphasis2"
+          ? "var(--cordis-black)"
+          : $type === "accent"
+          ? "var(--cordis-accent)"
+          : $type === "white"
+          ? "var(--cordis-white)"
+          : "var(--cordis-black)"};
     }
 
     outline: ${({ $type }) =>
       $type === "emphasis"
-      ? "2px solid var(--cordis-emphasis)"
-      : $type === "emphasis2"
-      ? "2px solid var(--cordis-black)"
-      : $type === "ghost"
-      ? "2px solid var(--cordis-black)"
-      : $type === "white"
-      ? "2px solid var(--cordis-white)"
-      : $type === "emphasis"
-      ? "2px solid var(--cordis-emphasis)"
-      : $type === "accent"
-      ? "2px solid var(--cordis-accent)"
-      : "none"};
+        ? "2px solid var(--cordis-emphasis)"
+        : $type === "emphasis2"
+        ? "2px solid var(--cordis-black)"
+        : $type === "ghost"
+        ? "2px solid var(--cordis-black)"
+        : $type === "white"
+        ? "2px solid var(--cordis-white)"
+        : $type === "emphasis"
+        ? "2px solid var(--cordis-emphasis)"
+        : $type === "accent"
+        ? "2px solid var(--cordis-accent)"
+        : "none"};
   }
 
   &:active {
@@ -109,50 +108,50 @@ const StyledButton = styled.button`
 
     background-color: ${({ $type }) =>
       $type === "accent-ghost"
-      ? "var(--cordis-accent)"
-      : $type === "underlined"
-      ? "var(--cordis-emphasis)"
-      : $type === "white-ghost"
-      ? "var(--cordis-white)"
-      : $type === "ghost"
-      ? "var(--cordis-black)"
-      : $type === "emphasis-ghost"
-      ? "var(--cordis-emphasis)"
-      : $type === "emphasis"
-      ? "rgba(255, 255, 255, 0)"
-      : "rgba(255, 255, 255, 0)"};
+        ? "var(--cordis-accent)"
+        : $type === "underlined"
+        ? "var(--cordis-emphasis)"
+        : $type === "white-ghost"
+        ? "var(--cordis-white)"
+        : $type === "ghost"
+        ? "var(--cordis-black)"
+        : $type === "emphasis-ghost"
+        ? "var(--cordis-emphasis)"
+        : $type === "emphasis"
+        ? "rgba(255, 255, 255, 0)"
+        : "rgba(255, 255, 255, 0)"};
 
     & p {
       color: ${({ $type }) =>
         $type === "accent-ghost"
-        ? "var(--cordis-black)"
-        : $type === "underlined"
-        ? "var(--cordis-black)"
-        : $type === "ghost"
-        ? "var(--cordis-white)"
-        : $type === "emphasis"
-        ? "var(--cordis-emphasis)"
-        : $type === "accent"
-        ? "var(--cordis-accent)"
-        : $type === "white"
-        ? "var(--cordis-white)"
-        : "var(--cordis-black)"};
+          ? "var(--cordis-black)"
+          : $type === "underlined"
+          ? "var(--cordis-black)"
+          : $type === "ghost"
+          ? "var(--cordis-white)"
+          : $type === "emphasis"
+          ? "var(--cordis-emphasis)"
+          : $type === "accent"
+          ? "var(--cordis-accent)"
+          : $type === "white"
+          ? "var(--cordis-white)"
+          : "var(--cordis-black)"};
     }
 
     outline: ${({ $type }) =>
       $type === "emphasis"
-      ? "2px solid var(--cordis-emphasis)"
-      : $type === "emphasis2"
-      ? "2px solid var(--cordis-black)"
-      : $type === "ghost"
-      ? "2px solid var(--cordis-black)"
-      : $type === "white"
-      ? "2px solid var(--cordis-white)"
-      : $type === "emphasis"
-      ? "2px solid var(--cordis-emphasis)"
-      : $type === "accent"
-      ? "2px solid var(--cordis-accent)"
-      : "none"};
+        ? "2px solid var(--cordis-emphasis)"
+        : $type === "emphasis2"
+        ? "2px solid var(--cordis-black)"
+        : $type === "ghost"
+        ? "2px solid var(--cordis-black)"
+        : $type === "white"
+        ? "2px solid var(--cordis-white)"
+        : $type === "emphasis"
+        ? "2px solid var(--cordis-emphasis)"
+        : $type === "accent"
+        ? "2px solid var(--cordis-accent)"
+        : "none"};
   }
 `;
 
