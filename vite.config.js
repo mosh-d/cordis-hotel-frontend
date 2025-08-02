3e84f9de37fc6687ea1037749a74c71ed421b82a
mosh-d <shopsy8@gmail.com>
2025-08-02 23:26:50 +0100

    Fix: Debugging Vercel deployment issues

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const isGitHubPages = process.env.GITHUB_PAGES === 'true';
+// const isGitHubPages = process.env.GITHUB_PAGES === 'true';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: isGitHubPages ? '/cordis-hotel-frontend/' : '/',
+  // base: isGitHubPages ? '/cordis-hotel-frontend/' : '/',
   plugins: [react()],
 })
